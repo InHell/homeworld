a38d00cd6a6f6718ad2754926ecdc5beb0e9e116
cyborg <cyffs@ukr.net>
2016-06-11 18:05:12 +0300

    ss2

--- dbs/CybersBase/src/ua/cybs/base/Main.java
+++ dbs/CybersBase/src/ua/cybs/base/Main.java
@@ -1,17 +1,50 @@
 package ua.cybs.base;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import ua.cybers.conectors.*;
+
 import ua.cybers.graph.Display;
 
 public class Main {
 
-	
+
 	
 	public static void main(String[] args) {
  
 	
 		
 		Display.bf();
+		// neeeeeext go on
 		
+		String query = "select count(*) from books";
+
+		try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+ 
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+ 
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
+ 
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                System.out.println("Total number of books in the table : " + count);
+            }
+ 
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
 		
 		
 
